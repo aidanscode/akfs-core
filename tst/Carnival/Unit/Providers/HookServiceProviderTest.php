eb9160db3209dc3358a2355f8c4d1c4fca484ad6
Kyle Evangelisto <kyleevangelisto@gmail.com>
2023-03-08 21:41:19 -0500

    Final PR feedback

--- tst/Carnival/Unit/Providers/HookServiceProviderTest.php
+++ tst/Carnival/Unit/Providers/HookServiceProviderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Carnival\Unit\Providers;
 
+use Carnival\Hooks\HookSystem;
 use Carnival\Providers\HookServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
 use Mockery;
@@ -12,7 +13,7 @@ class HookServiceProviderTest extends CarnivalTest {
     public function testHookServiceProviderCanRegisterHookLibraryFacade() {
         $application = Mockery::mock(Application::class);
         $hookServiceProvider = new HookServiceProvider($application);
-        $application->shouldReceive('bind');
+        $application->shouldReceive('bind')->with('hooks', Mockery::on(fn ($argument) => $argument() instanceof HookSystem ));
         $hookServiceProvider->register();
         $application->shouldHaveReceived('bind')->once();
     }
